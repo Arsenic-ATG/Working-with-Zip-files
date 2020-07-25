f3ca8e3e209c2b46d39866caee2836ca8b4e9d6c
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-25 10:38:34 +0530

    fix typos in messages

--- reading.py
+++ reading.py
@@ -1,10 +1,10 @@
 import zipfile,os
 
-os.chdir('location of the zip file you want to read')
+os.chdir('location of the folder in which the zip file exitsts')
 exampleZip = zipfile.ZipFile("name of the zip file you want to open")
 print(exampleZip.namelist())
 
-spam_info = exampleZip.getInfo('name of the subfile of the zip file you want informatino about the size')
+spam_info = exampleZip.getInfo('name of the subfile of the zip file you want information about the size')
 print(spam_info.file_size)
 print(spam_info.compress_size)
 exampleZip.close()
