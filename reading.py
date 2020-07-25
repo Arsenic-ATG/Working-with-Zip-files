3559064314189148149c91ee03df36b633f01751
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-25 10:37:19 +0530

    remove path and file name in reading file

--- reading.py
+++ reading.py
@@ -1,10 +1,10 @@
 import zipfile,os
 
-os.chdir('/Users/ankursaini/Desktop')
-exampleZip = zipfile.ZipFile("example.zip")
+os.chdir('location of the zip file you want to read')
+exampleZip = zipfile.ZipFile("name of the zip file you want to open")
 print(exampleZip.namelist())
 
-spam_info = exampleZip.getInfo('spam.txt')
+spam_info = exampleZip.getInfo('name of the subfile of the zip file you want informatino about the size')
 print(spam_info.file_size)
 print(spam_info.compress_size)
 exampleZip.close()
