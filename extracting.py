d62b95179c16121259be96df768ec4cd72314ddc
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-25 10:47:58 +0530

    add file name instruction

--- extracting.py
+++ extracting.py
@@ -1,6 +1,6 @@
 import os, zipfile
 
 os.chdir('location of the directory where zip file exits')
-example_zip = zipfile.ZipFile("example.zip")
+example_zip = zipfile.ZipFile("name of the zip file")
 example_zip.extractall()
 example_zip.close()
